4413333d1a44fc7e1e0b9e19adcced8f6aac50d3
trautlein <hans@trautlein.com>
2016-10-04 10:56:02 -0700

    Added client to folder and passed first five tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,4 +1,4 @@
-var qs = require('query-string');
+// var qs = require('query-string');
 /*************************************************************
 You should implement your request handler function in this file.
 requestHandler is already getting passed to http.createServer()
@@ -31,7 +31,7 @@ var baseObj = {
   results: []
 };
 
-module.exports = function(request, response) {
+var handler = function(request, response) {
   
   // Request and Response come from node's http module.
   //
@@ -66,8 +66,8 @@ module.exports = function(request, response) {
       };
       baseObj.results.unshift(dataObj);
       console.log(baseObj);
-      statusCode = 201;
-      response.writeHead(statusCode, headers);
+      response.writeHead(201, {'Content-Type': 'text/event-stream'});
+      response.end();
     });
     request.on('error', function(err) {
       console.log(err.stack);
@@ -82,7 +82,7 @@ module.exports = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  // headers['Content-Type'] = 'text/plain';
 
 
   if (request.method === 'GET') {
@@ -95,7 +95,7 @@ module.exports = function(request, response) {
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
 
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -104,5 +104,8 @@ module.exports = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // response.end('Hello, World!');
 };
+
+
+exports.handler = handler;
